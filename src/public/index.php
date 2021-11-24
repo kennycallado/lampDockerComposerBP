7a15c5ed91bbd727dbfda0d46f85e325f44ab38a
kenny callado <kennycallado@hotmail.com>
2021-11-24 19:16:50 +0100

    feature Added compatibility with twig Added dependencies to be able to manage twig extension like intl

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,20 @@
 <?php
 
+require_once "../vendor/autoload.php";
 
 echo "$_ENV[ENVIRONMENT]";
+
+/*
+ * Test for twig. Remember create the index.html in src/templates/
+ * mkdir templates/ && echo "<h1>hola mundo</h1><p>{{ '2019-08-07 23:00:00' | format_datetime(locale='es') }}</p>" > templates/index.html;
+ *
+ * use Twig\Loader\FilesystemLoader;
+ * use Twig\Environment;
+ * use Twig\Extra\Intl\IntlExtension;
+ * 
+ * $loader = new FilesystemLoader("../templates/");
+ * $twig = new Environment($loader);
+ * $twig->addExtension(new IntlExtension());
+ * 
+ * echo $twig->render("index.html");
+ */
